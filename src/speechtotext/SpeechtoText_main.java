73309d071b140ef13a97b3e93c837c8664d7c345
nykin <nykin@DESKTOP-FOJAC5C>
2018-06-07 21:13:59 +0900

    課題１

--- src/speechtotext/SpeechtoText_main.java
+++ src/speechtotext/SpeechtoText_main.java
@@ -16,33 +16,38 @@ public class SpeechtoText_main {
 	    SpeechToText service = new SpeechToText();
 	    service.setUsernameAndPassword("24a3fdd0-e405-49c1-9ba3-860b1b4f96aa", "1iAXxtQJo6cL");
 
-	    File audio = new File("./audio/audio-file.flac");
+	    File audio = new File("./audio/output.wav");
 	    RecognizeOptions options =null;
-		try {
+
+	    MySQL mysql = new MySQL();
+
+
+	    try {
 			options = new RecognizeOptions.Builder()
 					.model("ja-JP_BroadbandModel")
 			    .audio(audio)
-			    .contentType(RecognizeOptions.ContentType.AUDIO_FLAC)
+			    .contentType(RecognizeOptions.ContentType.AUDIO_WAV)
 			    .build();
 		} catch (FileNotFoundException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-	    SpeechRecognitionResults transcript = service.recognize(options).execute();
+	    SpeechRecognitionResults result = service.recognize(options).execute();
 
 	    JsonNode node=null;
 	    ObjectMapper mapper = new ObjectMapper();
 	    try {
-			 node = mapper.readTree(transcript.toString());
+			 node = mapper.readTree(result.toString());
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
+	    System.out.println(result);
+	    String transcript= node.get("results").get(0).get("alternatives").get(0).get("transcript").toString();
 	    System.out.println(transcript);
-	    String transcript1= node.get("results").get(0).get("alternatives").get(0).get("transcript").asText();
-	    System.out.println(transcript1);
 	    Double confidence = node.get("results").get(0).get("alternatives").get(0).get("confidence").asDouble();
 	    System.out.println(confidence);
+	    mysql.updateImage(transcript,confidence);
 
 	}
 
